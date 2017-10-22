9aba5fae0ec3d52f4fc35bdb01122bac83ebcd4f
heidihoge <heidihoge93@gmail.com>
2017-10-21 21:58:09 -0300

    DATABASE API ADMIN INTERFACE

--- productos/admin.py
+++ productos/admin.py
@@ -4,5 +4,8 @@
 from django.contrib import admin
 
 from .models import Producto
+from .models import Categoria
+
+admin.site.register(Categoria)
 
 admin.site.register(Producto)
--- productos/models.py
+++ productos/models.py
@@ -1,18 +1,28 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
 from django.db import models
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre_categoria
+
 
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=250)
     descripcion = models.CharField(max_length=250)
     precio = models.IntegerField(default=0)
-    categoria = models.CharField(max_length=250)
     producto_foto = models.CharField(max_length=1000)
+    categoria = models.ForeignKey(Categoria)
 
     def __str__(self):
-        return self.nombre_producto + ' - ' + str(self.precio)
+        return self.nombre_producto
 
 
 
-# class Categoria(models.Model):
-#     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-#     nombre_categoria = models.CharField (max_length= 250)
+
 
