fc896a3c6dd10b40a89c2a006925479e66ded5ab
heidihoge <heidihoge93@gmail.com>
2017-11-28 10:31:42 -0300

    Agrega ReportLab

--- productos/views.py
+++ productos/views.py
@@ -3,6 +3,7 @@ from productos.cruds import CategoriaCrud,ProductoCrud
 
 from django.http import HttpResponse
 from django.db.utils import OperationalError
+from reportlab.pdfgen import canvas
 
 try:
     builder = ViewBuilder('productos', 'producto', ProductoCrud)
@@ -14,6 +15,20 @@ except OperationalError:
          # importable without this side effect
 
 
-def index(request):
-    return HttpResponse ("<h1>Electiva3- Esta es la parte de productos <h1>")
 
+def report(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="reporte_productos.pdf"'
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Reporte productos")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
